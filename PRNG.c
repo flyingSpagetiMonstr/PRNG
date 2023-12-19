e622c4d17965ff4119e13dfc7d5612189d25f369
flyingSpagetiMonstr <2021302181087@whu.edu.cn>
2023-12-19 14:30:20 +0800

    cleaning

--- PRNG.c
+++ PRNG.c
@@ -13,7 +13,7 @@
 #define INFO "dumps/stream_len.dat"
 
 #define MILLION (1000000)
-#define STREAM_LEN (MILLION*1000)
+#define STREAM_LEN (MILLION*100)
 
 enum _init_method {
     zero_state, // = 0
@@ -26,22 +26,14 @@ enum _init_method {
 // P: 1567, G_most = 1565
 #define P 1567
 #define G 1565 // maximum primitive root
-// #define P 251
-// #define G 248 // maximum primitive root
 #define G_MULT(x) (((x)*G)%P)
 #define GRNG_ITER(x) (G_MULT(x))
 #define COMPRESS(x) ((uint8_t)(x))
-
-// #define USE(x) ((uint8_t)(x)) 
-// #define BOUND(x) ((x)%(P-1)+1)
-// #define GRNG_UPDATE(x) (x) = GRNG_ITER(x)
 // ==================================
 #define OP_N (4) // 2^{2}
-// #define TO_TWO (0b1)
 #define TO_FOUR (0b11)
 #define TO_EIGHT (0b111)
 #define PHI(x) (phi[(x)&TO_FOUR])
-// #define XCHG(a, b) a = a ^ b; b = a ^ b; a = a ^ b
 state_t *auxiliary = NULL;
 uint8_t xor(uint8_t x, uint8_t a) {return x ^ a;} 
 uint8_t add(uint8_t x, uint8_t a) {return x + a;}
@@ -49,11 +41,6 @@ uint8_t rshitf(uint8_t x, uint8_t a) {return RSHIFT(x, a&TO_EIGHT);}
 uint8_t unarys(uint8_t x, uint8_t a) {return a? ~x: auxiliary->f[x];}
 typedef uint8_t (*operation)(uint8_t, uint8_t);
 operation phi[OP_N] = {add, xor, rshitf, unarys};
-// uint8_t lshitf(uint8_t x, uint8_t a) {return LSHIFT(x, a&TO_EIGHT);}
-// uint8_t sub(uint8_t x, uint8_t a) {return x - a;}
-// uint8_t and(uint8_t x, uint8_t a) {return x & a;} // and is bad
-// uint8_t or(uint8_t x, uint8_t a) {return x | a;} // or is bad
-// uint8_t mul(uint8_t x, uint8_t a) {return x * a;} // mult is bad
 // ==================================
 
 void update(state_t* state);
@@ -63,11 +50,9 @@ void peak(state_t *state, int stream_len);
 int main()
 {
     uint8_t j = 0;
-    uint8_t bit = 0;
 
     state_t _s = {0};
     state_t *state = &_s;
-
     auxiliary = state;
 
     init_state(state, load_state);
@@ -76,7 +61,6 @@ int main()
 
     FILE *out = fopen(OUTPUT, "wb");
     int ajusted_stream_len = CEIL(STREAM_LEN, LEN*8); 
-
     ajusted_stream_len /= 8;
     
     puts("Generating...");
@@ -84,13 +68,8 @@ int main()
     for (uint64_t cnt = 0; cnt < ajusted_stream_len; cnt++)
     {
         j = state->f[i];
-
-        // bit = IS_ODD(j);
-        // if (bit_store(bit, state->bitstream) == 1)
-        //     fwrite(state->bitstream, 1, LEN, out);
-
         fwrite(&j, 1, 1, out);
-        update(state); // update f[i]
+        update(state); // mainly updates f[i]
         i = j;
 
         if ((cnt % (ajusted_stream_len/10)) == (ajusted_stream_len/10-1))
@@ -110,27 +89,12 @@ void update(state_t* state)
     #define f (state->f)
     #define index (state->i)
 
-    // ------------------------------------------
-    // register uint8_t new = 0;
-    // new = f[index] + index;
-    // new = phi[new&TO_FOUR](new, f[state->x]);
-    // new = phi[new&TO_FOUR](new, f[new]);
-    
-    // a = phi[b](a, c);
-    // a = phi[a&TO_FOUR](a, f[a]);
-
-    // for (register int cnt = 0; cnt < LEN; cnt++)
-    // {
-    //     new += f[cnt]; // gather
-    //     new += f[new]; // mix
-    // }
-    // ------------------------------------------
-    uint8_t new = 0;
     uint8_t old = f[index];
+
+    uint8_t new = 0;
     uint8_t register a = index;
     uint8_t register b = f[COMPRESS(a + state->x)];
     uint8_t register c = f[COMPRESS(state->x)];
-
     for (int cnt = 0; cnt < 22; cnt++)
     {
         a = PHI(b)(a, c);
@@ -140,7 +104,6 @@ void update(state_t* state)
         f[c] += b;
     }
     new = a;
-    // ------------------------------------------
 
     new += state->x;
     state->x = GRNG_ITER(state->x);
--- readme.md
+++ readme.md
@@ -5,3 +5,15 @@ NIST SP 800-22
 currently two versions: 
 - generating by bit: bit
 - generating by byte: master
+
+
+compiling with:  
+```
+  Using built-in specs.
+  COLLECT_GCC=C:\MinGW\bin\gcc.exe
+  COLLECT_LTO_WRAPPER=c:/mingw/bin/../libexec/gcc/x86_64-w64-mingw32/9.2.0/lto-wrapper.exe
+  Target: x86_64-w64-mingw32
+  Configured with: ../src/configure --enable-languages=c,c++ --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --disable-multilib --prefix=/c/temp/gcc/dest --with-sysroot=/c/temp/gcc/dest --disable-libstdcxx-pch --disable-libstdcxx-verbose --disable-nls --disable-shared --disable-win32-registry --with-tune=haswell --enable-threads=posix --enable-libgomp
+  Thread model: posix
+  gcc version 9.2.0 (GCC)
+```
